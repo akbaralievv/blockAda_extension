3c01445a515adef5604cce1a4598d5138055def5
AkbaralievAlisher <kgkgg28@gmail.com>
2023-10-12 00:31:20 +0600

    feat: add badge, reload, logo-off

--- popup/popup.js
+++ popup/popup.js
@@ -9,9 +9,23 @@ async function updateButtonState() {
   if (!isEnabled) {
     text.innerHTML = 'Disabled';
     wrapperPopup.classList.add('disabled');
+    chrome.action.setIcon({
+      path: {
+        16: '../assets/images/logo-off.png',
+        48: '../assets/images/logo-off.png',
+        128: '../assets/images/logo-off.png',
+      },
+    });
   } else {
     text.innerHTML = 'Enabled';
     wrapperPopup.classList.remove('disabled');
+    chrome.action.setIcon({
+      path: {
+        16: '../assets/images/logo.png',
+        48: '../assets/images/logo.png',
+        128: '../assets/images/logo.png',
+      },
+    });
   }
 }
 
